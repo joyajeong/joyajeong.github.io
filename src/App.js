c3e26298838bd538c596c239bda3d6d422a13fb7
joyaj <71417329+joyaj@users.noreply.github.com>
2021-02-27 14:41:02 +1100

    added buttons

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
+      <ul class='nav'>
+      <li><a class='active' href="home">Home</a></li>
+      <li><a href="contact">Contact Me</a></li>
+      </ul>
+      </header>
+      <div className="App-header">
         <h1 className="name">Joya Jeong</h1>
         <div className="intro">I'm a UNSW Information Systems/Commerce student that is an aspiring product
   and UI designer. Check out some of my work below!</div>
-      </header>
+      </div>
     </div>
   );
 }
