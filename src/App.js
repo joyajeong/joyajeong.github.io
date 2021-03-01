887a42cb592f3b6b03cff7d18f754be33f040b63
joyaj <71417329+joyaj@users.noreply.github.com>
2021-03-01 13:51:00 +1100

    redid npm install

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <ul class='nav'>
+      <li><a class='active' href="home">Home</a></li>
+      <li><a href="contact">Contact Me</a></li>
+      </ul>
+      </header>
+      <div className="App-header">
+        <h1 className="name">Joya Jeong</h1>
+        <div className="intro">I'm a UNSW Information Systems/Commerce student that is an aspiring product
+  and UI designer. Check out some of my work below!</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
