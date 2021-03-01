6b39a0e189799d7cc147e463693eaf439f41b26a
joyaj <71417329+joyaj@users.noreply.github.com>
2021-03-01 21:27:54 +1100

    .

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { HashRouter } from 'react-router-dom';
+import App from './App.js';
 
 ReactDOM.render(
     <React.StrictMode>
